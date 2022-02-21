99c8fb92c7aa7de9758783200ad70859d69107fd
nhungnguyenhuu <nhung.nguyen.hmu@gmail.com>
2022-02-21 08:16:51 +0700

    loi 404

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
-require "vendor/autoload.php";
 
 use App\Controller\AuthorsController;
 use App\Controller\BooksController;
 use App\Controller\GenreController;
 use App\Controller\PublishersController;
 use App\Controller\ReviewsController;
+use App\Controller\UsersController;
 
-$author = new AuthorsController;
-$publisher = new PublishersController;
-$review = new ReviewsController;
+require "vendor/autoload.php";
 
-$user = new UsersController;
-$page = $_GET["page"] ?? "";
+//use App\Controller\AuthorsController;
+//use App\Controller\BooksController;
+//use App\Controller\GenreController;
+//use App\Controller\PublishersController;
+//use App\Controller\ReviewsController;
+//use App\Controller\UsersController;
 
+$author = new AuthorsController();
+$publisher = new PublishersController();
+$review = new ReviewsController();
+$user = new UsersController();
 $book = new BooksController();
 $genre = new GenreController();
 
-$page = $_GET["page"] ?? "";
+//$author = new AuthorsController;
+//$publisher = new PublishersController;
+//$review = new ReviewsController;
+//$user = new UsersController;
+//
+//
+//$book = new BooksController();
+//$genre = new GenreController();
+
+
 $page =$_GET["page"] ?? "";
 
 ?>
@@ -29,8 +44,6 @@ $page =$_GET["page"] ?? "";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
-          integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
 <body>
 <a href="index.php?page=author-list">List</a>
@@ -41,45 +54,6 @@ $page =$_GET["page"] ?? "";
 <a href="index.php?page=publisher-list">Publishers List</a>
 <a href="index.php?page=review-list">Review List</a>
 <a href="index.php?page=user-list">User List</a>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="home.php">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                   aria-expanded="false">
-                    Menu
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="index.php?page=author-list">Author</a>
-                    <a class="dropdown-item" href="index.php?page=genre-list">Genres</a>
-                    <a class="dropdown-item" href="index.php?page=book-list">Books</a>
-                    <a class="dropdown-item" href="index.php?page=publisher-list">Publishers</a>
-                    <a class="dropdown-item" href="index.php?page=review-list">Review</a>
-                    <div class="dropdown-divider"></div>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </div>
-</nav>
 <!--<div class="jumbotron">-->
 <!--    <div class="container">-->
 <!--        <h1>Welcome to Nhã Nữ ( GAY's ) bookstore</h1>-->
@@ -97,16 +71,8 @@ switch ($page) {
         break;
     case "author-create":
 
-
         if ($_SERVER["REQUEST_METHOD"] == "GET") {
             $author->showFormCreate();
-        } else {
-
-
-        if($_SERVER["REQUEST_METHOD"] == "GET"){
-        if ($_SERVER["REQUEST_METHOD"] == "GET") {
-            $author->showFormCreate();
-        } else {
         }
         else{
 
@@ -122,13 +88,6 @@ switch ($page) {
         break;
     case "author-update":
         $author->updateAuthor($_POST, $_REQUEST["id"]);
-
-
-        $author->updateAuthor($_POST, $_REQUEST["id"]);
-
-
-        $author->updateAuthor($_POST,$_REQUEST["id"]);
-
 
         break;
 
@@ -149,15 +108,6 @@ switch ($page) {
         $publisher->updatePublisher($_POST, $_REQUEST["id"]);
         break;
 
-
-        $publisher->updatePublisher($_POST, $_REQUEST["id"]);
-
-
-        $publisher->updatePublisher($_POST,$_REQUEST["id"]);
-
-
-
-
     case "review-list":
         $review->showReview();
         break;
@@ -173,16 +123,6 @@ switch ($page) {
     case "review-edit":
         $review->edit();
         break;
-    case "review-create":
-        $review->create();
-        break;
-    case "review-detail":
-        $review->detail();
-        break;
-    case "review-edit":
-        $review->edit();
-        break;
-
 
     case "user-list":
         $user->showUser();
@@ -227,37 +167,16 @@ switch ($page) {
     case "book-delete":
         $book->delete();
         break;
-    case "genre-list":
-        $genre->getAll();
-        break;
-    case "genre-create":
-        $genre->create();
+
     case "book-create":
         $book->create();
         break;
-    case "genre-detail":
-        $genre->getById();
-    case "book-edit":
-        $book->edit();
-        break;
-    case "genre-edit":
-        $genre->edit();
-        break;
 
 
-    default:
+    default: header("location:index.php?page=book-list");
 
 
 }
 ?>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js"
-        integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2"
-        crossorigin="anonymous"></script>
 </body>
 </html>
